1d66d9082669cd1aa4869388cd1a3889f2a78682
Andrea Storci <s.andrea.storci@gmail.com>
2024-11-13 10:20:10 +0100

    Updated lesson 13/11/24

--- Metodologie Di Programmazione/Stack/inc/Stack.cpp
+++ Metodologie Di Programmazione/Stack/inc/Stack.cpp
@@ -3,6 +3,35 @@
 //
 
 #include "Stack.h"
+// #include <exception>
 
 namespace Memory {
+
+    using ErrorStack = std::invalid_argument;
+
+    // TRR = trailing return type
+    /*template<typename T>
+    inline auto Stack<T>::top() -> Stack<T>::top() const {
+
+    }*/
+
+    template<typename T>
+    Stack<T>::Stack() {
+        _capacity = 16;
+        _data = {};
+    }
+
+    template<typename T>
+    Stack<T>::Stack(size_t sz)
+    : _capacity(sz) {
+        if (sz < 16) {
+            throw ErrorStack("Invalid size type, cannot be less than 16");
+        }
+    }
+
+    template<typename T>
+    void Stack<T>::print(std::ostream &os, const Stack::value_type &o) {
+        os << "{";
+    }
+
 } // Memory
--- Metodologie Di Programmazione/Stack/inc/Stack.h
+++ Metodologie Di Programmazione/Stack/inc/Stack.h
@@ -3,12 +3,40 @@
 //
 #pragma once
 
+#include <iostream>
+#include <vector>
+
 namespace Memory {
 
     template<typename T>
     class Stack {
+    public:
+        // Alias for my parametric type
+        using value_type = T;
+        using vec = std::vector<T>;
+
+        using size_type = std::size_t;
+
+        Stack();
+
+        Stack(size_type st);
+
+        Stack(value_type&& o);
+
+        Stack(const value_type& o);
+
+        ~Stack();
+
+        void print(std::ostream& os, const value_type& o);
+
+        void scan(std::istream& is, value_type& o) const;
+
+        value_type& top() const;
+
     private:
-        
+        vec _data;
+        size_type _capacity;
+
     };
 
 } // Memory
